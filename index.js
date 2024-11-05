57c253201ae24fff7601dbd08d1419b9531a6d96
Char325 <111283967+Char325@users.noreply.github.com>
2024-11-05 23:00:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/status', (req, res) => {
-  res.json({ status: 'API is running' });
+  res.json({ status: 'API is running!' });
 });
 
 app.listen(port, () => {
