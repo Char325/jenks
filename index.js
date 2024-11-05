488b614d509361df82525d16bd98fdbcde279e9e
Char325 <111283967+Char325@users.noreply.github.com>
2024-11-05 22:58:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'API is running' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
